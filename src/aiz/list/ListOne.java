9a642fbb0d70831e12aed1081de061ba7e615232
JNoganski <noganski.jakub@gmail.com>
2020-12-20 12:28:09 +0100

    Zad1

---
+++ src/aiz/list/ListOne.java
@@ -0,0 +1,54 @@
+package aiz.list;
+
+
+public class ListOne implements IList {
+    @Override
+    public void addFirst(Object newData) {
+
+    }
+
+    @Override
+    public void addLast(Object newData) {
+
+    }
+
+    @Override
+    public void addAtPosition(Object newData, int position) throws ListException {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object removeFirst() throws ListException {
+        return null;
+    }
+
+    @Override
+    public Object removeLast() throws ListException {
+        return null;
+    }
+
+    @Override
+    public Object remove(int position) throws ListException {
+        return null;
+    }
+
+    @Override
+    public int find(Object dataToFind) {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object data) {
+        return false;
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
---
+++ src/aiz/list/Main.java
@@ -0,0 +1,8 @@
+package aiz.list;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
